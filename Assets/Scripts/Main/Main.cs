1141bed10f47a2685169152fd2a2785c06d549bd
sergme <sergiomorilloe@gmail.com>
2025-04-12 16:38:00 +0200

    Main

--- Assets/Scripts/Main/Main.cs
+++ Assets/Scripts/Main/Main.cs
@@ -3,9 +3,21 @@ using UnityEngine;
 public class Main : MonoBehaviour
 {
     private const string urlTrailer = "https://www.youtube.com/watch?v=YJ85K-zxAcE";
+    private const string urlGoogleAndroidStore = "https://play.google.com/store/apps/details?id=com.DanteGames.Hellmaster&pli=1";
+    private const string urlPCStore = "https://elixir.games/browse/hellmaster";
 
     public void OpenTrailer()
     {
         Application.OpenURL(urlTrailer);
+    }
+
+    public void OpenGoogleAndroidStore()
+    {
+        Application.OpenURL(urlGoogleAndroidStore);
+    }
+
+    public void OpenPC()
+    {
+        Application.OpenURL(urlPCStore);
     }
 }
