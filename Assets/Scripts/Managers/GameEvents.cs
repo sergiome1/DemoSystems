2faef1eefdd3e99119624c802fda290ccb3de860
sergme <sergiomorilloe@gmail.com>
2025-04-22 09:54:44 +0200

    GameEvents FIFO execution methods

--- Assets/Scripts/Managers/GameEvents.cs
+++ Assets/Scripts/Managers/GameEvents.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine;
 using static Assets.Scripts.GameplayElements.Card;
 
 namespace Assets.Scripts.Managers
@@ -15,5 +16,47 @@ namespace Assets.Scripts.Managers
         public static Action<TypeCardSelection, Suit, Value> ActivateSelectableCard;
 
         public static Action OnChangeLanguageDone;
+
+        public static IEnumerator RunEvents(List<Func<object[], IEnumerator>> actions, params object[] args)
+        {
+            for (int i = 0; i < actions.Count; i++)
+            {
+                if (actions[i] != null)
+                {
+                    yield return actions[i].Invoke(args);
+                }
+            }
+
+            for (int i = actions.Count - 1; i >= 0; i--)
+            {
+                if (actions[i] == null)
+                {
+                    actions.RemoveAt(i);
+                }
+            }
+        }
+
+        public static IEnumerator RunEvents(List<Action<object[], Action>> actions, params object[] args)
+        {
+            actions.Remove(null);
+
+            int toComplete = actions.Count;
+            int completedActions = 0;
+
+            void OnCompleteAction()
+            {
+                completedActions++;
+            }
+
+            for (int i = 0; i < actions.Count; i++)
+            {
+                if (actions[i] != null)
+                {
+                    actions[i].Invoke(args, OnCompleteAction);
+                }
+            }
+
+            yield return new WaitUntil(() => completedActions >= actions.Count);
+        }
     }
 }
